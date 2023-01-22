38b7247b234d670a19f052521f9790cf236e8d54
LenYx24 <fokilenard@gmail.com>
2023-01-22 22:11:25 +0100

    haha

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { useState, useEffect } from "react";
 import { CiClock2 } from "react-icons/ci";
 import type { ReactElement } from "react";
-import Layout from "../components/Layout.tsx";
+import Layout from "../components/Layout";
 import type { NextPageWithLayout } from "./_app";
 
 export interface user {
